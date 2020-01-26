3a8d62d0f43ba3668cb602241a7705aa7e23eb68
Olha Lytvynova-Bohdanova <olha@swarm64.com>
2020-01-26 17:37:07 +0100

    Solutions 1 and 2 done

---
+++ Solution_1.py
@@ -0,0 +1,76 @@
+import requests
+import copy
+
+
+documents_url = "https://raw.githubusercontent.com/omni-us/coding-challenges-resources/master/python-developer/api-response.json"
+
+response = requests.get(documents_url)
+documents_json = response.json()
+
+
+# Problem 1
+def get_docs_quantity_by_status():
+    
+    statuses_dict={}
+    try:
+        keys = []
+        values = []
+        for data_item in documents_json['payload']['items']:
+       
+            if data_item['status'] not in keys:
+                keys.append(data_item['status'])
+                values.append(1)
+            else:
+                item_index = keys.index(data_item['status'])
+                values[item_index] = values[item_index] + 1
+
+        for key, val in zip(keys, values):
+            statuses_dict[key]=val
+
+    except KeyError:
+        pass
+    finally:
+        return statuses_dict
+
+
+# Problem 2
+def get_doc_details_by_status(status):
+
+    status_details_list=[]
+    status_details_dict={}
+    try:
+        for data_item in documents_json['payload']['items']:
+            if data_item['status'] == status:
+                status_details_dict = copy.deepcopy(data_item)
+                del status_details_dict['status']
+                status_details_list.append(status_details_dict)
+        return(status_details_list)
+
+    except KeyError:
+        pass
+    finally:
+        return status_details_list
+
+
+# Problem 3
+def get_doc_details_by_filename(filename):
+
+    file_details_dict={}
+    try:
+        for data_item in documents_json['payload']['items']:
+            if data_item['file_name'] == filename:
+                file_details_dict = copy.deepcopy(data_item)
+                del file_details_dict['file_name']     
+                return(file_details_dict)
+
+    except KeyError:
+        pass
+    finally:
+        return file_details_dict
+
+
+if __name__ == "__main__":
+
+    get_docs_quantity_by_status()
+    get_doc_details_by_status('VALIDATED')
+    get_doc_details_by_filename('77.pdf')
---
+++ Solution_2.py
@@ -0,0 +1,53 @@
+import requests
+
+
+output_dict = {}
+
+def get_json_from_url(page_url):
+
+    response = requests.get(page_url)
+    page_json = response.json()
+   
+    return page_json
+
+
+def add_value(key, value):
+  
+    try:
+        if key != '':
+            if key not in output_dict:
+                output_dict.update({key: [value]})
+            else:
+                output_dict[key].append(value)
+    except KeyError:
+        pass
+
+
+def out_flatten(dict_value, str_value):
+
+    if type(dict_value) is dict: 
+        for key in dict_value.keys():
+            newKey = key
+            if str_value: 
+                newKey = str_value + '_' + key
+            add_value(str_value, newKey)       
+            out_flatten(dict_value[key], newKey) 
+    elif type(dict_value) is list or type(dict_value) is set:
+        for item in dict_value:
+            out_flatten(item, str_value + '_' + item)
+    else:
+        add_value(str_value, dict_value)
+
+    return output_dict
+
+
+if __name__ == "__main__":
+
+    json = get_json_from_url("https://raw.githubusercontent.com/omni-us/coding-challenges-resources/master/python-developer/sample-1.json")
+    out_flatten(json, '')
+
+    json = get_json_from_url("https://raw.githubusercontent.com/omni-us/coding-challenges-resources/master/python-developer/sample-2.json")
+    out_flatten(json, '')
+
+    json = get_json_from_url("https://raw.githubusercontent.com/omni-us/coding-challenges-resources/master/python-developer/sample-3.json")
+    out_flatten(json, '')
